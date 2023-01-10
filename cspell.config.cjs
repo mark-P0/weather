930d9bc0800d9697c964906d01fe08449fa0e451
Mark P <mj.poso18@gmail.com>
2023-01-10 15:52:01 +0800

    Setup ESLint, Prettier, CSpell

---
+++ cspell.config.cjs
@@ -0,0 +1,10 @@
+/* prettier-ignore */
+const words = `
+`.trim().split('\n');
+
+/** @type {import('@cspell/cspell-types').CSpellSettings} */
+const config = {
+  words,
+};
+
+module.exports = config;

9bf9cd95d505987f9c23a93e5424997617fc0bbd
Mark P <mj.poso18@gmail.com>
2023-01-12 09:45:34 +0800

    Minify output CSS with CSSNano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,12 @@
 /**
  * https://tailwindcss.com/docs/installation/using-postcss
+ * https://tailwindcss.com/docs/optimizing-for-production
  */
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    /* TODO: Only add this on production builds? */
+    cssnano: {},
   },
 };

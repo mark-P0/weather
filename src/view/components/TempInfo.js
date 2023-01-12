679eebec1be53265083b5519e6f285b81622692c
Mark P <mj.poso18@gmail.com>
2023-01-12 11:50:12 +0800

    Inherit temperature toggle text colors

--- src/view/components/TempInfo.js
+++ src/view/components/TempInfo.js
@@ -86,13 +86,13 @@ function Radio(name, text, isDefault = false) {
     {
       class: [
         'text-sm text-stone-400 font-thin border-transparent border-2 px-1 text-center h-full aspect-square leading-none',
-        'hover:text-black hover:border-inherit',
+        'hover:text-inherit hover:border-inherit',
         /**
          * As of January 2023, the `:has()` is NOT supported on Firefox,
          * as well as VSCode's browser preview.
          * https://developer.mozilla.org/en-US/docs/Web/CSS/:has#browser_compatibility
          */
-        '[&:has(input:checked)]:text-black [&:has(input:checked)]:font-bold',
+        '[&:has(input:checked)]:text-inherit [&:has(input:checked)]:font-bold',
       ].join(' '),
     },
     { type: 'radio', name, class: 'appearance-none' },

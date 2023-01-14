d5ae58879e883540aabac0e4db0e62d9ca50c5de
Mark P <mj.poso18@gmail.com>
2023-01-14 08:58:02 +0800

    Add GIPHY to footer

--- src/view/components/Disclaimer.js
+++ src/view/components/Disclaimer.js
@@ -1,12 +1,19 @@
 import { E } from '../dom.js';
 
+/** @type {(text: string, href: string, classes: string) => HTMLElement} */
+function Link(text, href, classes) {
+  return E('a', text, {
+    href,
+    target: '_blank',
+    class: `font-medium hover:underline ${classes}`,
+  });
+}
+
 export function Disclaimer() {
   return E('p', { class: 'text-xs text-stone-400 text-center font-thin' }, [
     'Powered by ',
-    E('a', 'OpenWeatherMap', {
-      href: 'https://openweathermap.org/',
-      target: '_blank',
-      class: 'font-medium text-orange-500 hover:underline',
-    }),
+    Link('OpenWeatherMap', 'https://openweathermap.org/', 'text-orange-500'),
+    ' & ',
+    Link('GIPHY', 'https://giphy.com/', 'text-emerald-400'),
   ]);
 }
